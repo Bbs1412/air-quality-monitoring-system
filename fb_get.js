a0d369b4a36ec22f52d5874397883178e05f3e68
Bhushan Songire <bhushanbsongire@gmail.com>
2024-06-12 22:28:58 +0530

    test_trynerr6

--- fb_get.js
+++ fb_get.js
@@ -3,7 +3,8 @@ console.log("FBget: Firebase dependency file loaded!!!");
 
 // Function to fetch configuration from the serverless function
 const getConfig = async () => {
-    const response = await fetch('/api/fb_env.js');
+    // const response = await fetch('/api/fb_env.js');
+    const response = await fetch('/api/fb_env');
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
